b7107a5ef2989b81a15f67b7f5423678c6725e3e
Analía Río <analia.rio@gmail.com>
2022-03-29 11:28:50 +0200

    Final version

--- js/main.js
+++ js/main.js
@@ -1,42 +1,45 @@
 "use strict";
 
 // 1. CONSTANTS
-// To select HTML elements - document.querySelector
 const oneToSix = document.querySelector(".js-onetosix");
 const wager = document.querySelector(".js-howmuch");
 const btn = document.querySelector(".btn");
 const output = document.queryCommandIndeterm(".output");
 const balance = document.querySelector(".balance");
 
-// Values
-// const chosenNo = oneToSix.value;
-// const wagerValue = wager.value;
-const balanceValue = balance.value;
-
 // 2. FUNCTIONS
-// Function to generate random number
+// Function to generate random no
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
+// Function to compare user-chosen no with random no
 function checkNo() {
 
+    // Get values
     const chosenNoString = oneToSix.value;
-    const wagerValue = wager.value;
+    const wagerValueString = wager.value;
+    const balanceValueString = balance.value;
+    
+    // Get random no value
     const randomNo = getRandomNumber(6);
 
+    // Turn values from string type into no type
     const chosenNo = parseInt(chosenNoString);
+    const balanceValue = parseInt(balanceValueString); 
+    const wagerValue = parseInt(wagerValueString); 
 
     console.log(chosenNo);
     console.log(randomNo);
     if (chosenNo === randomNo) {
         // Same number - Double bet
-        output.innerHTML = "Has ganado el doble de lo apostado 😃";
-        //wagerValue
+        output.innerHTML = `<p>Has ganado el doble de lo apostado 😃</p>`;
+        balanceValue = wagerValue + wagerValue;
      
     } else {
         // Different number - Lost
-        output.innerHTML = "Has perdido lo apostado 😦";
+        output.innerHTML = `<p>Has perdido lo apostado 😦</p>`;
+        balanceValue = 0;
     }
 }
 
